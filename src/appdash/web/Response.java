a67326405fbba683b975e86a5a04767d624fd429
Alex Osborne <aosborne@nla.gov.au>
2018-08-03 11:16:32 +0900

    Remove unused imports

--- src/appdash/web/Response.java
+++ src/appdash/web/Response.java
@@ -1,13 +1,7 @@
 package appdash.web;
 
-import io.undertow.io.DefaultIoCallback;
-import io.undertow.io.Sender;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
-import org.xnio.IoUtils;
-
-import java.io.IOException;
-import java.nio.channels.FileChannel;
 
 public interface Response {
     void send(Request request) throws Exception;
